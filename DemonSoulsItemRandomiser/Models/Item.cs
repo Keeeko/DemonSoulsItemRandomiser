75bcaac2d2b9a69ca01ae6bacb500696b5d3a2af
keeeko <kjmotherwell@gmail.com>
2020-04-27 20:26:46 +0100

    Add project files.

---
+++ DemonSoulsItemRandomiser/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemonSoulsItemRandomiser.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+
+        public Item(int iD)
+        {
+            ID = iD;
+        }
+    }
+}
